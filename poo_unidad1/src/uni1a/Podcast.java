0948d3fe98497469fda67d7d7ad89175e8b6e1ae
denissemosquera633-max <denissemosquera633@gmail.com>
2025-08-03 14:46:37 -0500

    Podcast

--- poo_unidad1/src/uni1a/Podcast.java
+++ poo_unidad1/src/uni1a/Podcast.java
@@ -1,17 +1,16 @@
 package uni1a;
 
-// Subclase Podcast que extiende de ContenidoAudiovisual
 public class Podcast extends ContenidoAudiovisual {
     private String host;
     private String plataforma;
 
+    // CONSTRUCTOR CORREGIDO: ahora recibe 'plataforma'
     public Podcast(String titulo, int duracionEnMinutos, String genero, String host, String plataforma) {
         super(titulo, duracionEnMinutos, genero);
         this.host = host;
         this.plataforma = plataforma;
     }
 
-    // Getters y setters
     public String getHost() {
         return host;
     }
@@ -28,14 +27,15 @@ public class Podcast extends ContenidoAudiovisual {
         this.plataforma = plataforma;
     }
 
+    // METODO CORREGIDO: devuelve un String en lugar de imprimir
     @Override
-    public void mostrarDetalles() {
-        System.out.println("Detalles del Podcast:");
-        System.out.println("ID: " + getId());
-        System.out.println("Título: " + getTitulo());
-        System.out.println("Duración en minutos: " + getDuracionEnMinutos());
-        System.out.println("Género: " + getGenero());
-        System.out.println("Host: " + this.host);
-        System.out.println("Plataforma: " + this.plataforma);
+    public String mostrarDetalles() {
+        return "Detalles del Podcast:\n" +
+                "ID: " + getId() + "\n" +
+                "Título: " + getTitulo() + "\n" +
+                "Duración en minutos: " + getDuracionEnMinutos() + "\n" +
+                "Género: " + getGenero() + "\n" +
+                "Host: " + this.host + "\n" +
+                "Plataforma: " + this.plataforma;
     }
 }
