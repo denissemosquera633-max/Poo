b2b70d99dbd65a0415cbe0529f5dcc01824e0ae0
denissemosquera633-max <denissemosquera633@gmail.com>
2025-08-03 14:47:03 -0500

    SerieDeTV

--- poo_unidad1/src/uni1a/SerieDeTV.java
+++ poo_unidad1/src/uni1a/SerieDeTV.java
@@ -3,16 +3,15 @@ package uni1a;
 import java.util.ArrayList;
 import java.util.List;
 
-// Subclase SerieDeTV que extiende de ContenidoAudiovisual
 public class SerieDeTV extends ContenidoAudiovisual {
-    private List<Temporada> temporadas; // Relación de composición: una serie tiene temporadas
+    private List<Temporada> temporadas;
 
+    // CONSTRUCTOR CORREGIDO: se eliminó el parámetro 'i'
     public SerieDeTV(String titulo, int duracionEnMinutos, String genero) {
         super(titulo, duracionEnMinutos, genero);
         this.temporadas = new ArrayList<>();
     }
 
-    // Getters y setters
     public List<Temporada> getTemporadas() {
         return temporadas;
     }
@@ -21,17 +20,20 @@ public class SerieDeTV extends ContenidoAudiovisual {
         this.temporadas.add(temporada);
     }
 
+    // METODO CORREGIDO: devuelve un String en lugar de imprimir
     @Override
-    public void mostrarDetalles() {
-        System.out.println("Detalles de la Serie de TV:");
-        System.out.println("ID: " + getId());
-        System.out.println("Título: " + getTitulo());
-        System.out.println("Duración de episodio: " + getDuracionEnMinutos() + " minutos");
-        System.out.println("Género: " + getGenero());
-        System.out.println("Número de temporadas: " + this.temporadas.size());
-        System.out.println("Temporadas: ");
+    public String mostrarDetalles() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Detalles de la Serie de TV:\n");
+        sb.append("ID: ").append(getId()).append("\n");
+        sb.append("Título: ").append(getTitulo()).append("\n");
+        sb.append("Duración de episodio: ").append(getDuracionEnMinutos()).append(" minutos\n");
+        sb.append("Género: ").append(getGenero()).append("\n");
+        sb.append("Número de temporadas: ").append(this.temporadas.size()).append("\n");
+        sb.append("Temporadas: \n");
         for (Temporada temporada : temporadas) {
-            System.out.println("  - " + temporada);
+            sb.append("  - ").append(temporada.toString()).append("\n");
         }
+        return sb.toString();
     }
 }
